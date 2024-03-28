0949f42945336810b86c820c595454a4303c55c2
gurubbc <gurumurthy.ramamurthy@gmail.com>
2024-03-28 12:52:03 +0000

    Create HelloWorld.java

---
+++ src/com/cfgbank/HelloWorld.java
@@ -0,0 +1,7 @@
+package com.cfgbank;
+
+public class HelloWorld {
+  public static void main(String args[]) {
+    System.out.println("just testing....assume that this file was pushed by another developer");
+  }
+}
